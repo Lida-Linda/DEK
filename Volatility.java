bcbca4236969c33e9e38dfdc829e2ead65aaa3a8
Lida-Linda <e_cyber@ukr.net>
2016-05-29 14:27:45 +0300

    Update Volatility.java

--- Volatility.java
+++ Volatility.java
@@ -1,7 +1,13 @@
 import java.io;
 import java.lang;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+
 
 public class Volatility
 {
-  
+  public static void main (String[] args)
+  {
+    
+  }
 }
