fbe80b1f48916900530d096c825a62cab5d68bd8
Lida-Linda <e_cyber@ukr.net>
2016-05-30 12:57:07 +0300

    Update Volatility.java

--- Volatility.java
+++ Volatility.java
@@ -8,43 +8,59 @@ import java.util.ArrayList;
 
 public class Volatility
 {
-  private static List<Character> chars = new ArrayList<>();
   
-  public static void main (String[] args)
-  {
+  public void write(String st) throws IOException {
+    // открываем поток ввода в файл
+   outputStream = new FileOutputStream(path);
+ 
+    // записываем данные в файл, но 
+    // пока еще данные не попадут в файл,
+    // а просто будут в памяти
+    outputStream.write(st.getBytes());
+    // только после закрытия потока записи, 
+    // данные попадают в файл
+    outputStream.close();
+  }
+ private static List<Character> chars = new ArrayList<>();
+  
+  public static void main (String[] args) {
     BufferedReader reader = null;
-    try 
-    {
+    
+    try {
       reader = new BufferedReader(new FileReader(new File("data.txt")));
       int c;
-      while ((c = reader.read()) != -1) // Коли дійдемо до кінця файлу отримаємо -1
-      {
+      while ((c = reader.read()) != -1) { // Коли дійдемо до кінця файлу отримаємо -1
         chars.add((char) c);
       }
       reader.close();
     } 
-    catch (IOException e) 
-    {
+    
+    catch (IOException e) {
       e.printStackTrace();
     } 
-    finally 
-    {
-      if (reader != null) 
-      {
-        try 
-        {
+    
+    finally {
+      if (reader != null) {
+        try {
           reader.close();
         }
-        catch (IOException e) 
-        {
+        
+        catch (IOException e) {
           e.printStackTrace();
         }
-    }
+      }
+      //створюмо файл
+      File newFile = new File("Volatility.txt");
+      //формула
+     double g,a;
+     int m=50; //вікно
+     for (c = 1; c <= args.length-1; c++) {
+        g[c]=Math.log(a[c+1])-Math.log(a[c]); //прибутковості
+        
+        //write()
+     }
     
     //FileInputStream myfile = new FileInputStream("data.txt");
-    цикл...
-    reader
-    волатильність
     
   }
 }
