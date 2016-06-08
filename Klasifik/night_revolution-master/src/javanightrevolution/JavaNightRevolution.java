8996c5b5b99605831d1b49b98ee43713523af05f
Lida-Linda <e_cyber@ukr.net>
2016-06-08 03:02:57 +0300

    Add files via upload

---
+++ Klasifik/night_revolution-master/src/javanightrevolution/JavaNightRevolution.java
@@ -0,0 +1,191 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javanightrevolution;
+
+import javafx.scene.layout.GridPane;
+import java.util.*;
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.HBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+import javafx.stage.FileChooser;
+import javafx.util.converter.FloatStringConverter;
+
+import java.io.FileWriter;
+import java.io.File;
+import java.io.IOException;
+import java.awt.Desktop;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.Arrays;
+import java.util.List;
+import java.util.ArrayList;
+
+
+/**
+ *
+ * @author admin
+ */
+public class JavaNightRevolution extends Application {
+    private Desktop desktop = Desktop.getDesktop();
+    private List<Float> raw_data = new ArrayList<Float>();
+    private List<Double> result;
+
+    @Override
+    public void start(Stage primaryStage) {
+        final FileChooser fileChooser = new FileChooser();
+
+        Label label1 = new Label("Result: ");
+        TextField resultField = new TextField ();
+        HBox resultBox = new HBox();
+        resultBox.getChildren().addAll(label1, resultField);
+        resultBox.setSpacing(10);
+        
+        Button openFile = new Button();
+        openFile.setText("Open file");
+        openFile.setOnAction(new EventHandler<ActionEvent>() {
+  
+            @Override
+            public void handle(ActionEvent event) {
+                File file = fileChooser.showOpenDialog(primaryStage);
+                if (file != null) {
+                    openFile(file);
+                    result = calculate();
+                    String formatted_string = new String("[");
+                    for (final Double element : result) {
+                       formatted_string += Double.toString(element);
+                    }
+                    formatted_string += "]";
+                    resultField.setText(formatted_string);
+                }
+            }
+        });
+        
+        Button writeFile = new Button();
+        writeFile.setText("Write file");
+        writeFile.setOnAction(new EventHandler<ActionEvent>() {
+            
+            @Override
+            public void handle(ActionEvent event) {
+                FileChooser fileChooser = new FileChooser();
+                fileChooser.setTitle("Save calculated result");
+                File file = fileChooser.showSaveDialog(primaryStage);
+//                writeStringToFile(file, resultField.getText());
+                writeArrayToFile(file, result);
+            }
+        });
+        
+        
+        StackPane root = new StackPane();
+        
+        // show buttons 
+        root.getChildren().add(resultBox);
+        final GridPane inputGridPane = new GridPane();
+        GridPane.setConstraints(openFile, 0, 5);
+        GridPane.setConstraints(writeFile, 1, 5);
+        inputGridPane.setHgap(6);
+        inputGridPane.setVgap(6);
+        inputGridPane.getChildren().addAll(openFile, writeFile);
+        root.getChildren().add(inputGridPane);
+
+        Scene scene = new Scene(root, 300, 250);
+        
+        primaryStage.setTitle("Hey you! =)");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+    private void openFile(File file) {
+        try {
+            FileReader fileReader = new FileReader(file);
+            BufferedReader br = new BufferedReader(fileReader);
+            String line = null;
+
+            while ((line = br.readLine()) != null) {
+                float value = Float.parseFloat(line);
+                raw_data.add(value);
+            }
+            br.close();
+        } catch (IOException ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
+    
+    private List<Double> calculate() {
+        List<Double> result = new ArrayList();
+        double wind = 25; //change
+        double num_classes = 10; // к-ть класів
+        double sum_s = 0; //сума для сер.значення
+        double zero = 0;
+        List<Double> pr = new ArrayList();
+        List<Double> norm = new ArrayList();
+        for(int n = 0; n < raw_data.size() - 1; n++) {
+           pr.add(Math.log(raw_data.get(n + 1)) - Math.log(raw_data.get(n))); //прибутковості 
+           sum_s += pr.get(n);
+        }
+        double ser_pr = sum_s / (raw_data.size() - 2); //середнє
+        
+        double sum=0, S;
+    for (int i=0;i<raw_data.size()-1;i++)
+     {
+       sum= sum+(pr.get(i)-ser_pr)*(pr.get(i)-ser_pr);
+     }
+     S=Math.sqrt(sum/(raw_data.size()-1));
+    for (int i=0;i<raw_data.size()-1;i++){
+      norm.add((pr.get(i)-ser_pr)/S);//нормалізовані
+    }
+    for (int i=0;i<raw_data.size()-1;i++)
+      {
+        double v = Math.round(((num_classes - 1) * (norm.get(i) + 3))/6);  
+        if (v >= 1 && v <= num_classes - 1) result.add(v);
+        else result.add(zero);
+      }
+        return result;
+    }
+    
+    
+    private void writeStringToFile(File file, String result) {
+        if (file != null) {
+            try {
+                FileWriter wr = new FileWriter(file);
+                wr.write(result);
+                wr.close();
+                desktop.open(file);
+            } catch (IOException ex) {
+                System.out.println(ex.getMessage());
+            }
+        }
+    }
+    
+    private void writeArrayToFile(File file, List<Double> resultList) {
+        if (file != null) {
+            try {
+                FileWriter writer = new FileWriter(file);
+                for (Double item : resultList) {
+                    writer.write(Double.toString(item));
+                    writer.write(System.lineSeparator());
+                }
+                writer.close();
+                desktop.open(file);
+            } catch (IOException ex) {
+                System.out.println(ex.getMessage());
+            }
+        }
+    }  
+}
---
+++ autocor/night_revolution-master/src/javanightrevolution/JavaNightRevolution.java
@@ -0,0 +1,184 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javanightrevolution;
+
+import javafx.scene.layout.GridPane;
+import java.util.*;
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.HBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+import javafx.stage.FileChooser;
+import javafx.util.converter.FloatStringConverter;
+
+import java.io.FileWriter;
+import java.io.File;
+import java.io.IOException;
+import java.awt.Desktop;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.Arrays;
+import java.util.List;
+import java.util.ArrayList;
+
+
+/**
+ *
+ * @author admin
+ */
+public class JavaNightRevolution extends Application {
+    private Desktop desktop = Desktop.getDesktop();
+    private List<Float> raw_data = new ArrayList<Float>();
+    private List<Double> result;
+
+    @Override
+    public void start(Stage primaryStage) {
+        final FileChooser fileChooser = new FileChooser();
+
+        Label label1 = new Label("Result: ");
+        TextField resultField = new TextField ();
+        HBox resultBox = new HBox();
+        resultBox.getChildren().addAll(label1, resultField);
+        resultBox.setSpacing(10);
+        
+        Button openFile = new Button();
+        openFile.setText("Open file");
+        openFile.setOnAction(new EventHandler<ActionEvent>() {
+  
+            @Override
+            public void handle(ActionEvent event) {
+                File file = fileChooser.showOpenDialog(primaryStage);
+                if (file != null) {
+                    openFile(file);
+                    result = calculate();
+                    String formatted_string = new String("[");
+                    for (final Double element : result) {
+                       formatted_string += Double.toString(element);
+                    }
+                    formatted_string += "]";
+                    resultField.setText(formatted_string);
+                }
+            }
+        });
+        
+        Button writeFile = new Button();
+        writeFile.setText("Write file");
+        writeFile.setOnAction(new EventHandler<ActionEvent>() {
+            
+            @Override
+            public void handle(ActionEvent event) {
+                FileChooser fileChooser = new FileChooser();
+                fileChooser.setTitle("Save calculated result");
+                File file = fileChooser.showSaveDialog(primaryStage);
+//                writeStringToFile(file, resultField.getText());
+                writeArrayToFile(file, result);
+            }
+        });
+        
+        
+        StackPane root = new StackPane();
+        
+        // show buttons 
+        root.getChildren().add(resultBox);
+        final GridPane inputGridPane = new GridPane();
+        GridPane.setConstraints(openFile, 0, 5);
+        GridPane.setConstraints(writeFile, 1, 5);
+        inputGridPane.setHgap(6);
+        inputGridPane.setVgap(6);
+        inputGridPane.getChildren().addAll(openFile, writeFile);
+        root.getChildren().add(inputGridPane);
+
+        Scene scene = new Scene(root, 300, 250);
+        
+        primaryStage.setTitle("Hey you! =)");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+    private void openFile(File file) {
+        try {
+            FileReader fileReader = new FileReader(file);
+            BufferedReader br = new BufferedReader(fileReader);
+            String line = null;
+
+            while ((line = br.readLine()) != null) {
+                float value = Float.parseFloat(line);
+                raw_data.add(value);
+            }
+            br.close();
+        } catch (IOException ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
+    
+    private List<Double> calculate() {
+        List<Double> result = new ArrayList();
+        double sum1 = 0, sum2 = 0;
+        double lag = 25; //change lag
+        List<Double> pr = new ArrayList();
+        
+        for (int n = 0; n < raw_data.size() - 1; n++) {
+           sum1 += raw_data.get(n); // for M      
+        } 
+        double m = (1 / (raw_data.size() - 1) * sum1);// M
+        
+        for (int q = 0; q <= raw_data.size() - 1; q++) {
+           sum2 += Math.pow((raw_data.get(q) - m), 2); // D
+        }
+        
+        for (int k = 0; k <= raw_data.size() - lag - 1; k++){
+            double sum3 = 0;
+            for (int j = k; j <= k + lag - 1; j++) {
+                sum3 += ((raw_data.get(j) - m) * ((raw_data.get(j) + lag) - m));
+            }
+            result.add(sum3 / sum2); //автокореляція
+        } 
+        return result;
+    }
+    
+    
+    private void writeStringToFile(File file, String result) {
+        if (file != null) {
+            try {
+                FileWriter wr = new FileWriter(file);
+                wr.write(result);
+                wr.close();
+                desktop.open(file);
+            } catch (IOException ex) {
+                System.out.println(ex.getMessage());
+            }
+        }
+    }
+    
+    private void writeArrayToFile(File file, List<Double> resultList) {
+        if (file != null) {
+            try {
+                FileWriter writer = new FileWriter(file);
+                for (Double item : resultList) {
+                    writer.write(Double.toString(item));
+                    writer.write(System.lineSeparator());
+                }
+                writer.close();
+                desktop.open(file);
+            } catch (IOException ex) {
+                System.out.println(ex.getMessage());
+            }
+        }
+    }  
+}
